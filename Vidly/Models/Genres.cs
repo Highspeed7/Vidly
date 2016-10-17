cba3c60d104e29b8a26f4e38c4280f73403bd27e
Highspeed7 <b.sanders7777@edmail.edcc.edu>
2016-10-16 20:06:17 -0700

    Removing Migrations

---
+++ Vidly/Models/Genres.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vidly.Models
+{
+    public class Genres
+    {
+        [Required]
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
